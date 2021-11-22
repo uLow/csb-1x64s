08c15bf29614c7a9c7d15d2132af27e93bfc92b3
Vitalijs Litvinovs <dev@codin.pro>
2021-11-22 10:44:13 +0200

    Update precache-manifest.ab9a39398f489f70d87fcd7554cd8abe.js

--- precache-manifest.ab9a39398f489f70d87fcd7554cd8abe.js
+++ precache-manifest.ab9a39398f489f70d87fcd7554cd8abe.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "bfe809caded82e264f808dd807966894",
-    "url": "https://.github.io/csb-1x64s/index.html"
+    "url": "https://ulow.github.io/csb-1x64s/index.html"
   },
   {
     "revision": "7260ddaa0b0b3ef5b0ac",
-    "url": "https://.github.io/csb-1x64s/static/css/main.14b6c901.chunk.css"
+    "url": "https://ulow.github.io/csb-1x64s/static/css/main.14b6c901.chunk.css"
   },
   {
     "revision": "5c75ad81fe0e9960d4b6",
-    "url": "https://.github.io/csb-1x64s/static/js/2.a8230a98.chunk.js"
+    "url": "https://ulow.github.io/csb-1x64s/static/js/2.a8230a98.chunk.js"
   },
   {
     "revision": "7260ddaa0b0b3ef5b0ac",
-    "url": "https://.github.io/csb-1x64s/static/js/main.f991131a.chunk.js"
+    "url": "https://ulow.github.io/csb-1x64s/static/js/main.f991131a.chunk.js"
   },
   {
     "revision": "4d4ede64b0c10d7ed3d0",
-    "url": "https://.github.io/csb-1x64s/static/js/runtime~main.3f0f5236.js"
+    "url": "https://ulow.github.io/csb-1x64s/static/js/runtime~main.3f0f5236.js"
   }
 ]);
